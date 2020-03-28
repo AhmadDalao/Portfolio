362f8fcb24c4ee035d82260b2bf1c66e8a02381e
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-03-28 23:33:27 +0300

    you can download the resume now

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ showSlides();
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -12,7 +13,11 @@ function showSlides() {
     if (slideIndex > slides.length) {
         slideIndex = 1
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
