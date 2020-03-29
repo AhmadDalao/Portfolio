78c4487dafcf7b0bd66c4b71ff8fb4c9408ebe1c
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-03-29 21:33:44 +0300

    php form

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "ahmodmyd@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
