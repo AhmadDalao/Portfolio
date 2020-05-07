a54049f975491437437c323952757d542c0b128a
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-05-07 23:33:28 +0300

    adding smooth

--- js/main.js
+++ js/main.js
@@ -86,6 +86,18 @@ resume = () => {
     }, 4000);
 };
 
+
+// smooth scroll
+function scrollTo() {
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
+            link.onclick = scrollAnchors;
+        }
+    }
+}
+
 // automatic slideshow
 // var slideIndex = 0;
 // showSlides();
