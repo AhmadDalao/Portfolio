bbce162987e28a083150d4798f4df5916ef27449
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-05-08 04:36:12 +0300

    made improvements to the skill section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("navbar-not-responsive");
+    if (x.className === "navbar-list") {
+        x.className += " responsive";
+        document.getElementById("my-iconX").style.display = "block";
+        document.getElementById("my-icon").style.display = "none";
+
+    } else {
+        x.className = "navbar-list";
+        document.getElementById("my-iconX").style.display = "none";
+        document.getElementById("my-icon").style.display = "block";
+    }
+}
+
 // automatic and manuel
 
 var slideIndex = 1;
