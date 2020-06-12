0acdbffc0bb54e8c77d5e47e4f650079c36da848
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-06-12 14:57:36 +0300

    code refactor

--- js/main.js
+++ js/main.js
@@ -1,171 +1,78 @@
-function myFunction() {
-    var x = document.getElementById("navbar-not-responsive");
-    if (x.className === "navbar-list") {
-        x.className += " responsive";
-        document.getElementById("my-iconX").style.display = "block";
-        document.getElementById("my-icon").style.display = "none";
+var myNav = document.getElementById('navbar'),
+    icon = document.querySelector('.my-container'),
+    myColor = document.querySelectorAll('.color-change'),
+    list;
 
-    } else {
-        x.className = "navbar-list";
-        document.getElementById("my-iconX").style.display = "none";
-        document.getElementById("my-icon").style.display = "block";
-    }
+/* this is  the custom attribute name  which value is to be changed */
+const changeMe = "--main-color";
+/* setting the color from the localStorage  by retrieving */
+
+document.documentElement.style.setProperty(changeMe, localStorage.getItem("colorStored"));
+
+
+/*  getting value of  */
+list = document.querySelectorAll('[data-color]');
+console.log(list.length);
+
+for (var i = 0; i < list.length; ++i) {
+    list[i].addEventListener("click", function(e) {
+        // store attribute value in localStorage
+        localStorage.setItem("colorStored", e.target.getAttribute('data-color'));
+        console.log("attribute value " + e.target.getAttribute('data-color'));
+
+        // change color based on the attribute value.
+        document.documentElement.style.setProperty(changeMe, e.target.getAttribute('data-color'));
+        console.log("My local storage value " + localStorage.getItem("colorStored"));
+
+    }, false);
 }
 
-// automatic and manuel
-
-var slideIndex = 1;
-
-var myTimer;
-
-var slideshowContainer;
-
-window.addEventListener("load", function() {
-    showSlides(slideIndex);
-    myTimer = setInterval(function() {
-        plusSlides(1);
-    }, 4000);
-
-    //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-    slideshowContainer = document.getElementsByClassName("slideshow-inner")[0];
-
-    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-    slideshowContainer = document.getElementsByClassName(
-        "slideshow-container"
-    )[0];
-
-    slideshowContainer.addEventListener("mouseenter", pause);
-    slideshowContainer.addEventListener("mouseleave", resume);
+/* side icon */
+document.getElementById('color-toggler').addEventListener('click', function() {
+    document.getElementById('color-container').classList.toggle('show');
 });
 
-// NEXT AND PREVIOUS CONTROL
-function plusSlides(n) {
-    clearInterval(myTimer);
-    if (n < 0) {
-        showSlides((slideIndex -= 1));
+
+/* make navbar responsive and change to x icon */
+function myFunction() {
+    document.querySelector(".navbar-not-responsive").classList.toggle('responsive');
+    document.querySelector('.my-container').classList.toggle('my-button');
+}
+
+/* if the screen height is below 550 and you click on the bar icon the navbar will get background if it was transparent */
+myNav.addEventListener('click', function() {
+    if (document.body.scrollTop < 550 && document.documentElement.scrollTop < 550) {
+        if (myNav.className === "nav-transparent" || myNav.className === "nav-colored");
+        myNav.classList.add('nav-colored');
+    }
+});
+
+/* if the screen height is below 550 and you hover over the bar icon the navbar will get background if it was transparent */
+myNav.addEventListener('mousemove', function() {
+    if (document.body.scrollTop < 550 && document.documentElement.scrollTop < 550) {
+        myNav.classList.remove('nav-transparent');
+        myNav.classList.add('nav-colored');
+    }
+});
+
+/* if the screen height is below 550 and you hover out,  the navbar will get back to transparent */
+myNav.addEventListener('mouseout', function() {
+    if (document.body.scrollTop < 550 && document.documentElement.scrollTop < 550) {
+        myNav.classList.remove('nav-colored');
+        myNav.classList.add('nav-transparent');
+
+    }
+});
+
+
+/* if scrolling add the background color to the navbar */
+window.onscroll = function() {
+    "use strict";
+    if (document.body.scrollTop >= 250 || document.documentElement.scrollTop >= 250) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
     } else {
-        showSlides((slideIndex += 1));
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
     }
-
-    // COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-
-    if (n === -1) {
-        myTimer = setInterval(function() {
-            plusSlides(n + 2);
-        }, 4000);
-    } else {
-        myTimer = setInterval(function() {
-            plusSlides(n + 1);
-        }, 4000);
-    }
-}
-
-//Controls the current slide and resets interval if needed
-function currentSlide(n) {
-    clearInterval(myTimer);
-    myTimer = setInterval(function() {
-        plusSlides(n + 1);
-    }, 4000);
-    showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-pause = () => {
-    clearInterval(myTimer);
 };
-
-resume = () => {
-    clearInterval(myTimer);
-    myTimer = setInterval(function() {
-        plusSlides(slideIndex);
-    }, 4000);
-};
-
-
-// smooth scroll
-function scrollTo() {
-    var links = document.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-        if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
-            link.onclick = scrollAnchors;
-        }
-    }
-}
-
-// automatic slideshow
-// var slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var dots = document.getElementsByClassName("dot");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {
-//         slideIndex = 1
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " active";
-//     setTimeout(showSlides, 4000); // Change image every 4 seconds
-// }
-
-// fixed slideshow
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var dots = document.getElementsByClassName("dot");
-//     if (n > slides.length) {
-//         slideIndex = 1
-//     }
-//     if (n < 1) {
-//         slideIndex = slides.length
-//     }
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " active";
-//
-// }
-// }
