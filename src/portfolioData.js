adfdc6f09535f0a153234d6de982b841ade034a9
AhmadDalao <shadow8evil@gmail.com>
2021-03-23 07:08:31 +0300

    updating about me paragraph

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -222,13 +222,7 @@ const SkillsImages = [
 const AboutSection = {
   id: "about",
   title: "about me",
-  paragraph: ` I'm a Software Engineer / Developer who loves building and developing
-          applications and websites. I'd love to combine my passion for
-          programming and learning with my software engineering skills to
-          continue building more personalized applications and websites for
-          people. I have a passion to learn everything new, contributing to the
-          progress of mankind, and show creativity and possibilities in
-          everything I do.`,
+  paragraph: `✨ I'm a Software Engineer / Developer 👨🏻‍💻 who loves building and developing applications and websites. I'd love to combine my passion for programming and learning with my software engineering skills to continue building 🧑‍💻 more personalized applications and websites for people . I have a passion to learn everything new ❤, contributing to the progress of mankind, and show creativity and possibilities in everything I do ✨.`,
 };
 const AboutServices = {
   title: "services",
